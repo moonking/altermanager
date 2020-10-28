bcca47030ad67eda3aa4619998d3f3b752c5716d
migoooooo <18095541887@163.com>
2020-10-28 21:18:35 +0800

    '249:8075,test_bms'

--- src/config/prod.js
+++ src/config/prod.js
@@ -4,10 +4,14 @@
  * @date 2019/6/28
  */
 module.exports = {
-  aiaUrl: 'http://111.231.165.249:8086/',
-  baserUrl: 'http://111.231.165.249:8086/',
-  CiBaseUrl: 'http://111.231.165.249:8087/',
+  aiaUrl: 'http://111.231.165.249:8075/test_bms/aia/',
+  baserUrl: 'http://111.231.165.249:8075/test_bms/',
+  CiBaseUrl: 'http://111.231.165.249:8075/test_cmdb/',
   wsTopology: 'ws://111.231.165.249:8087/ws/topology/ciitem/'
+  // aiaUrl: 'http://111.231.165.249:8086/',
+  // baserUrl: 'http://111.231.165.249:8086/',
+  // CiBaseUrl: 'http://111.231.165.249:8087/',
+  // wsTopology: 'ws://111.231.165.249:8087/ws/topology/ciitem/'
   // deliveryUrl: 'http://10.131.111.167:8075/delivery/',
   // devopsUrl: 'http://10.131.111.167:8075/devops/',
   // workflowUrl: 'http://10.131.111.167:8091/',
