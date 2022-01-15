95d1cbb6e410bee3d7772ae8217f09889cfbc798
moonking <hanliang_bit@163.com>
2022-01-15 19:15:38 +0800

    delete src

--- src/src/api/index.js
+++
@@ -1,7 +0,0 @@
-// 接口整合
-import * as bmsApi from './apis/bmsApi';
-import * as cmdbApi from './apis/cmdbApi';
-import * as aibmsApi from './apis/aibmsApi';
-import * as workflowApi from './apis/workflowApi';
-const apiObject = Object.assign({}, bmsApi, cmdbApi, workflowApi, aibmsApi);
-export default apiObject;
--- src/src/components/common/bus.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-// import router from '../../router'
-// // 使用 Event Bus
-// Vue.use(router)
-const bus = new Vue()
-
-export default bus
